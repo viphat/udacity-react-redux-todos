b2093e9847dc95bd3861b75f6281d6a506711419
Duong Vi Phat <viphat@gmail.com>
2018-05-14 19:56:07 +0700

    First Commit

---
+++ index.js
@@ -0,0 +1,63 @@
+// Library Code
+function createStore(reducer) {
+  // The Store has 4 parts:
+  // 1. The State.
+  // 2. Get the State.
+  // 3. Listen to changes on the state.
+  // 4. Update the state
+
+  let state;
+  let listeners = [];
+
+  const getState = () => state
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+    return () => {
+      listeners = listeners.filter((l) => l !== listener)
+    }
+  }
+
+  const dispatch = (action) => {
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener());
+  }
+
+  return {
+    getState,
+    subscribe,
+    dispatch
+  }
+}
+
+// App Code
+
+function todos(state = [], action) {
+  switch(action.type) {
+    case 'ADD_TODO' :
+      return state.concat([action.todo]);
+    case 'REMOVE_TODO' :
+      return state.filter((todo) => todo.id !== action.id);
+    case 'TOGGLE_TODO':
+      return state.map((todo) => todo.id !== action.id ? todo :
+        Object.asign({}, todo, { complete: !todo.complete })
+      );
+    default:
+      return state;
+  }
+}
+
+const store = createStore(todos);
+
+store.subscribe(() => {
+  console.log('The new state is: ', store.getState())
+});
+
+store.dispatch({
+  type: 'ADD_TODO',
+  todo: {
+    id: 0,
+    name: 'Learn Redux',
+    complete: false
+  }
+});
